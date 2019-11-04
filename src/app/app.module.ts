581e128a5ffd05209ffa0512f1cc94aca559d3bd
Marie Schumacher <marie.schumacher@carano.de>
2019-11-04 14:35:38 +0100

    added Angular Material & dummy table, removed package-lock.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import {MatTableModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
