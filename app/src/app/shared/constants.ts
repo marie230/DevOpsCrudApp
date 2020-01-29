3ae17a67180d8d4ca80ccf91dfc28e248586c8be
Marie Schumacher <marie.schumacher@carano.de>
2020-01-29 20:53:05 +0100

    * changed environment variables

--- app/src/app/shared/constants.ts
+++ app/src/app/shared/constants.ts
@@ -1,1 +1,5 @@
-export const BASE_API_URL = 'http://localhost:1234/api/heroes/';
+// use 'localhost' environment URL for testing locally
+
+// export const ENVIRONMENT_URL = 'localhost';
+export const ENVIRONMENT_URL = window.location.href.includes('development') ? '192.168.33.27' : '192.168.33.28';
+export const BASE_API_URL = `http://${ENVIRONMENT_URL}:1234/api/heroes/`;
